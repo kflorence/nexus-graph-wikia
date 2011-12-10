422055d66758c9a0255c5501cf190bd9760485f6
Kyle Florence <kyle.florence@gmail.com>
2011-12-09 16:45:15 -0800

    Top 5 wiki pages now working

--- main.js
+++ main.js
@@ -8,8 +8,51 @@
  */
 
 jQuery(function($) {
-	nexus.init({ api: 'http://naruto.kflorence.wikia-dev.com/api.php', canvas: '#nexusGraph' });
+	var wiki = window.location.search.replace('?', '') || 'naruto';
 
-	nexus.system.addNode("test");
-	nexus.system.addNode("test2");
+	nexus.init({ api: 'http://' + wiki + '.kflorence.wikia-dev.com/api.php', canvas: 'nexusGraph' });
+
+	$.ajax({
+		url: nexus.data.api,
+		data: {
+			action: 'query',
+			list: 'wkpoppages',
+			wklimit: 5,
+			format: 'json'
+		},
+		dataType: 'jsonp',
+		success: function(data) {
+			$.each(data.query.wkpoppages, function(id, page) {
+				$.ajax({
+					url: nexus.data.api,
+					data: {
+						action: 'query',
+						prop: 'links',
+						titles: page.title,
+						format: 'json'
+					},
+					dataType: 'jsonp',
+					success: function(data) {
+						var title = data.query.pages[id].title;
+						nexus.data.pages[title] = nexus.system.addNode(title, page);
+	
+						$.each(data.query.pages[id].links, function(id, link) {
+							if (!nexus.data.pages[link.title]) {
+								nexus.data.pages[link.title] = nexus.system.addNode(link.title);
+							}
+
+							// Link this node to the page node
+							nexus.system.addEdge(link.title, title);
+						});
+					},
+					error: function() {
+						console.log('error: ', arguments);
+					}
+				});
+			});
+		},
+		error: function() {
+			console.log('error: ', arguments);
+		}
+	});
 });
